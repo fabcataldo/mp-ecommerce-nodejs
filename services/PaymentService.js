0b1cdb3ed83155b2b760d8435fa2d07100fcc05d
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-23 18:31:28 -0300

    Sigo limpiando código.

--- services/PaymentService.js
+++ services/PaymentService.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 class PaymentService {
     constructor() {
         this.tokensMercadoPago = {
-            prod: {},
             test: {
                 access_token:
                     "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"
