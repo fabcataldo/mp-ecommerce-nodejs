ed3140bf259f1ccb777a90dd4cbfe4b607dbb862
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-23 18:21:03 -0300

    Borro código

--- app.js
+++ app.js
@@ -23,13 +23,6 @@ app.set('view engine', 'handlebars');
 app.use('/assets', express.static(__dirname + '/assets'));
 
 
-const mercadopago = require ('mercadopago');
-mercadopago.configure({
-    integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
-    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
-});
-
-
 var payer={
     id: "471923173",
     name:"Lalo",
