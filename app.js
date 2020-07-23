2f1813f884de469a95c92e8b8bb1196f9942df58
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-23 17:51:36 -0300

    Limpio y mejoro código.

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var cors = require('cors')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+const PaymentController = require("./controllers/PaymentController");
+const PaymentService = require("./services/PaymentService"); 
+const PaymentControllerObj = new PaymentController(new PaymentService()); 
+var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(express.static('assets'));
+app.use(express.json());
+//app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs());
+app.use(cookieParser());
+app.use(logger('dev'));
+app.set('view engine', 'handlebars');
+app.use('/assets', express.static(__dirname + '/assets'));
+
+
 const mercadopago = require ('mercadopago');
 mercadopago.configure({
-    access_token: 'TEST-4810749615636979-022920-32ef3bbc2b8d5497662fe21661515b92-233164701'
-  });
- 
-var app = express();
-app.use(cors())
-// configurar cabeceras http
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
-    next();
+    integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
+    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
 });
 
-var preferences;
+
 var payer={
+    id: "471923173",
+    type: "registered",
     name:"Lalo",
     surname:"Landa",
     email:"test_user_63274575@testuser.com",
@@ -25,15 +40,22 @@ var payer={
         area_code:"11",
         number:"22223333"
     },
+    identification: {
+        type: "DNI",
+        number: "12345678"
+    },
     address:{
         zip_code:"1111",
         street_name:"False",
         street_number:123
     }
 }
+var preferences = {
+    payer: payer
+};
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+
+
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -44,39 +66,35 @@ app.get('/pendingPayment', function(req,res){
 })
 
 app.get('/successPayment', function(req,res){
-    res.render('successPayment');
+    res.render('successPayment', req.query);
 })
-
 
 app.get('/failurePayment', function(req,res){
     res.render('failurePayment');
 })
 
+app.post("/webhook", (req, res) => {
+    PaymentControllerObj.webhook(req,res);
+});
+
 app.post('/procesar-pago', (req,res)=>{
-    mercadopago.preferences.create(preferences)
-    .then(function(response){
-    // Este valor reemplazará el string "$$init_point$$" en tu HTML
-    global.init_point = response.body.init_point;
-    console.log(response)
-    }).catch(function(error){
-        console.log(error)
-    console.log(error);
-    });
+    PaymentControllerObj.getMercadoPagoLink(preferences, req ,res);    
 })
 
 app.get('/detail', function (req, res) {
-    console.log(req.query)
-    preferences = {
-        items:[{
+    preferences.items = [
+        {
+            id: "1234",
             title: req.query.title,
             description: "Dispositivo móvil de Tienda e-commerce",
             img: req.query.img,
             quantity: parseInt(req.query.unit),
+            category_id: "1234",
+            currency_id: "ARS",
             unit_price: parseFloat(req.query.price)
             }
-        ],
-        payer:payer,
-        payment_methods:{
+    ];
+    preferences.payment_methods = {
             excluded_payment_methods:[
                 {
                     id:'amex',
@@ -87,29 +105,19 @@ app.get('/detail', function (req, res) {
                     id:'atm'
                 }
             ],
-            installments: 6
-        },
-        notification_url:'http://localhost:3000/',
-        back_urls:{
-            success:'http://localhost:3000/successPayment?collection_id=[PAYMENT_ID]'+
-            '&collection_status=approved&external_reference=[EXTERNAL_REFERENCE]'+
-            '&payment_type=credit_card&preference_id=[PREFERENCE_ID]&site_id'+
-            '=[SITE_ID]&processing_mode=aggregator&merchant_account_id=null',
-            pending:'http://localhost:3000/pendingPayment',
-            failure:'http://localhost:3000/failurePayment'
-        },
-        external_reference: "fabioalbertocataldo@gmail.com"
-    };
+            installments: 6,
+            default_installments: 6
+    }
+    preferences.notification_url ='https://fabcataldo-mp-commerce-nodejs.herokuapp.com/webhook';
+    preferences.back_urls = {
+            success:'https://localhost:3000/successPayment',
+            pending:'https://localhost:3000/pendingPayment',
+            failure:'https://localhost:3000/failurePayment'
+    }
+    preferences.auto_return =  'approved';
+    preferences.external_reference = "fabioalbertocataldo@gmail.com";
     res.render('detail', req.query);
 });
 
-app.use(express.static('assets'));
-
-app.use('/assets', express.static(__dirname + '/assets'));
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
 app.listen(3000);
 
